320bd70472374637d6830e3cb02d13f0b60582c8
Chris N Pena <Cpena1@Chriss-MacBook-Pro.local>
2016-03-28 00:10:25 -0500

    Dev Coffee got can now connect.

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 // Get our tmi module
 var tmi = require('tmi.js');
 
+// Readline tool to prompt user for channel name
+var readline = require('readline');
+var rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
 // Get API Key from module
 // Goto api.twitch.tv to get yours
 var apiKey = require('./key.js');
@@ -17,5 +24,34 @@ var options = {
 	identity: {
 		username: "DevCoffeeBot",
 		password: apiKey
-	}
+	},
+	channels: ["devcoffee_"]
 }
+
+
+//Connect to client;
+var client = new tmi.client(options);
+client.connect();
+client.on("chat", function(channel, user, message, self){
+	client.color("Red").then(function(data) {
+		client.action("devcoffee_", "Suh dude");
+	}).catch(function(err) {
+	   
+	});
+	client.color("GoldenRod").then(function(data) {
+		client.action("devcoffee_", "Suh dude");
+	}).catch(function(err) {
+	   
+	});
+	client.color("HotPink").then(function(data) {
+		client.action("devcoffee_", "Suh dude");
+	}).catch(function(err) {
+	   
+	});
+});
+
+// Uncomment later
+// var channelName = rl.question("What channel do you want me in?", function(answer) {
+// 	rl.close();
+// 	options.channels.push("#" + answer);
+// });
